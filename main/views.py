a4aa67be55db4128f00c05b5e2cd07347f1f8652
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2021-04-04 23:14:56 +0300

    Separate Search Pages
    Added separate search and result pages.

--- main/views.py
+++ main/views.py
@@ -6,8 +6,11 @@ import json
 
 def home(request):
     return render(request, 'main/home.html')
- 
+
 def search(request):
+    return render(request, 'main/search.html')
+ 
+def search_results(request):
     all_items = []
     page_num = 1
     url = "https://www.fashiondays.bg/g/%D0%9C%D1%8A%D0%B6%D0%B5-superdry/%D0%94%D1%80%D0%B5%D1%85%D0%B8-%D0%A1%D1%83%D0%B8%D1%82%D1%88%D1%8A%D1%80%D1%82%D0%B8_%D1%81_%D0%BA%D0%B0%D1%87%D1%83%D0%BB%D0%BA%D0%B0"
@@ -72,4 +75,4 @@ def search(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, 'main/search.html', context)
+    return render(request, 'main/search-results.html', context)
