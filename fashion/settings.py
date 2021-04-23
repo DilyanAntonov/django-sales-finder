a803636a749d70dc596ce65062565060ef9ac4b5
Dilyan Antonov <dilyan.y.antonov@gmail.com>
2021-04-23 15:58:59 +0300

    Settings Fix

--- fashion/settings.py
+++ fashion/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0&54@%apc9c3r=fm!q_!ktm4d34wzf1@s^65wzqdm$nrw8m#so'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['dajangosalesfinder.herokuapp.com']
+ALLOWED_HOSTS = ['dajangosalesfinder.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
